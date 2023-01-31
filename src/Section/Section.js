8499ff5f23fc643b541ebd953e3a079134f2be1f
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2023-01-31 16:24:30 +0100

    probleme de synthaxe

--- src/Section/Section.js
+++ src/Section/Section.js
@@ -1,11 +1,14 @@
 import datasTab from "../App";
 
-const Section = (datasTab) => {
-  return (
-    
-      datasTab.map(elem=>return(
-        <div> {elem.category}</div>));
-    
-  );
+const Section = ({ datasTab }) => {
+  console.log("dataTab:", datasTab);
+  return(<div>
+  {datasTab.map(elem=>return(<h2>{elem.category}</h2>) 
+  elem.images.map element=>return(<img src="element"alt="image contenu netflix"/>)
+)}
+
+  </div>
+  )
+
 };
 export default Section;
